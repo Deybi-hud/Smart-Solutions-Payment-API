8aef12674bf0f4674939ee423f66ea22456bb3cf
Deybi-hud <deyb.ramirez@duocuc.cl>
2026-06-13 18:08:49 -0400

    feat: Implementación de health

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
   console.log(`📡 Ambiente: ${process.env.NODE_ENV || 'development'}`);
 });
